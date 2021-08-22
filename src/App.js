cff22404b3a5f06dd9ada78682aeb0d9e5125265
CodeWithHarry <48705673+CodeWithHarry@users.noreply.github.com>
2021-08-23 00:53:17 +0530

    Video 18 Completed

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import Alert from './components/Alert';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
  
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,7 @@ export default function Navbar(props) {
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/about">{props.aboutText}</Link>
