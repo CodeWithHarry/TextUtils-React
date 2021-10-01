33dab0abde2c7b3b1880c8c3cc87da2741c1b076
Padmanabh <70370670+Padmanabh82@users.noreply.github.com>
2021-10-01 17:23:57 +0530

    add now function "Fix Grammer Mistakes'

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
             <About mode={mode} />
           </Route>
           <Route exact path="/">
-            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
+            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces and Much more" mode={mode}/>
           </Route>
     </Switch>
     </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,32 +1,28 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 
 export default function TextForm(props) {
-    const handleUpClick = ()=>{
+    const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText)
         props.showAlert("Converted to uppercase!", "success");
     }
 
-    const handleLoClick = ()=>{ 
+    const handleLoClick = () => {
         let newText = text.toLowerCase();
         setText(newText)
         props.showAlert("Converted to lowercase!", "success");
     }
 
-    const handleClearClick = ()=>{ 
+    const handleClearClick = () => {
         let newText = '';
         setText(newText);
         props.showAlert("Text Cleared!", "success");
     }
-
-    const handleOnChange = (event)=>{
-        setText(event.target.value) 
-    }
-
     // Credits: A
     const handleCopy = () => {
-        navigator.clipboard.writeText(text); 
+        navigator.clipboard.writeText(text);
         props.showAlert("Copied to Clipboard!", "success");
     }
 
@@ -37,29 +33,52 @@ export default function TextForm(props) {
         props.showAlert("Extra spaces removed!", "success");
     }
 
-    const [text, setText] = useState(''); 
+    // Credits: Padmanabh82
+    let handleGrammerCheck = () => {
+        let data = text.split(/[ ]+/).join("+");
+        //you need to get your api key from https://textgears.com to use this function.
+        axios({
+            url: `https://api.textgears.com/grammar?text=${data}!&language=en-GB&whitelist=&dictionary_id=&key=${process.env.REACT_APP_API_KEY}`
+        })
+            .then(function (response) {
+                if (response.data.discription) {
+                    props.showAlert("Plese set your key of api.Get it from https://textgears.com!", "error");
+                }
+                else {
+                    let Checked_text = text
+                    for (const i in response.data.response.errors) {
+                        Checked_text = Checked_text.replace(response.data.response.errors[i].bad, response.data.response.errors[i].better[0])
+                    }
+                    setText(Checked_text)
+                    props.showAlert("Your Grammer Mistakes has been Fixed!", "success");
+                }
+            }).catch(function (error) {
+                console.log(error)
+            })
+    }
+
+    const [text, setText] = useState('');
     // text = "new text"; // Wrong way to change the state
     // setText("new text"); // Correct way to change the state
     return (
         <>
-        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
-            <h1 className='mb-4'>{props.heading}</h1>
-            <div className="mb-3"> 
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+            <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+                <h1 className='mb-4'>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onInput={event => { setText(event.target.value) }} style={{ backgroundColor: props.mode === 'dark' ? '#13466e' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="8"></textarea>
+                </div>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleGrammerCheck}>Fix Grammer mistakes</button>
             </div>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
-            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-        </div>
-        <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
-            <h2>Your text summary</h2>
-            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
-            <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
-            <h2>Preview</h2>
-            <p>{text.length>0?text:"Nothing to preview!"}</p>
-        </div>
+            <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+                <h2>Your text summary</h2>
+                <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} Minutes read</p>
+            </div>
         </>
     )
 }
