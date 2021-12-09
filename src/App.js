f98af1a4c62c8a2076db74613f1e4758fc898cf9
Anasdevs <68700511+Anasdevs@users.noreply.github.com>
2021-12-09 23:54:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,12 +29,12 @@ function App() {
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success");
+      showAlert("Dark mode is enabled now", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
+      showAlert("Light mode is enabled now", "success");
     }
   }
   return (
